f74063947fcc8230cc54d5d8f229024d281d41a6
borgg9 <52835335+borgg9@users.noreply.github.com>
2020-12-05 11:34:47 +0100

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Bucle_for_factorial {
+    
+    public static void main(String[] args){
+        
+        // Número para calcular su factorail
+        int num_calcular = Integer.parseInt(JOptionPane.showInputDialog("faltorial de ? "));
+
+        int factorial = 1;
+        for (int i = num_calcular; i > 0; i--){
+            factorial *= i;
+        }
+        System.out.println("El factorial de " + num_calcular + " es " + factorial + ".");
+    }  
+}
